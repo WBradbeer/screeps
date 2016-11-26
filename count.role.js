7258cabf0ac374c56918e6d0d13de73554a85ef4
Will Bradbeer <willbradbeer@gmail.com>
2016-11-26 08:46:27 -0500

    Added safegaurd if no harvesters.

--- count.role.js
+++ count.role.js
@@ -5,9 +5,15 @@ var countRole = {
             console.log(role + 's: ' + inRole.length);
             
             if(inRole.length < data[role]) {
-                var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {'role': role});
+                var newName = Game.spawns['Spawn1'].createCreep([WORK, WORK, WORK, CARRY, MOVE], undefined, {'role': role});
                 console.log('Spawning new ' + role + ': ' + newName);
             }
+            if(role == 'harvester' && inRole.length == 0) {
+                creep = _.filter(Game.creeps, (creep) => creep.memory.role != role)[0];
+                creep.memory.role = role
+                console.log('Converting ' + creep.name + ' to harvester.')
+            }
+
         }
     }
 };
