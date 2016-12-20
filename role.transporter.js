013998ef66063e4dfea7f88f83f0052d155f0911
Will Bradbeer <willbradbeer@gmail.com>
2016-12-19 21:24:48 -0500

    Use storage as savings.

--- role.transporter.js
+++ role.transporter.js
@@ -40,7 +40,8 @@ var roleTransporter = {
                     filter: (structure) => {
                         return ((structure.structureType == STRUCTURE_EXTENSION ||
                                 structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER
+                                structure.structureType == STRUCTURE_TOWER ||
+                                structure.structureType == STRUCTURE_STORAGE
                                 ) && structure.energy < structure.energyCapacity);
                     }
             });
